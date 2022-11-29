a224fd9ac7fe9006612507c4c146bd18f0bd590e
Titor <foolsecret@163.com>
2022-11-30 02:11:22 +0800

    Publish: 2.0.0

--- README.md
+++ README.md
@@ -1,23 +1,34 @@
-<link rel="stylesheet" href="/css/swiss.css">
-
-
 # BeeMarkdown
 简单轻巧的Markdown文档在线预览工具.
 
-### Useage
+## Useage
 
-启动md服务
+###### 启动md服务
 ``` bash
 md start [port]端口号,默认2222
 ```
 
-初始化md
+###### 初始化md
 ``` bash
 md init
 ```
 
-其它命令
-``` bash
-md --help     显示帮助信息
-md --version  显示md版本号
+
+###### 其它命令
+```shell
+md --help     (显示帮助信息)
+md --version  (显示md版本号)
 ```
+
+## 更新历史
+<dl>
+  <dt>11/30 2022 <em><sup>NEW</sup></em><dt>
+  <dd>
+
+  - 使用了[@Titor-Z/markdown-theme][theme]来替换系统原先的默认 **theme**。
+  - 优化了默认的文档流输出，添加了完善的html输出标准。
+  - 版本发布为 <em>2.0.0</em>
+  </dd>
+</dl>
+
+[theme]: https://github.com/Titor-Z/markdown-theme/pkgs/npm/markdown-theme "Markdown Theme"
--- src/server.ts
+++ src/server.ts
@@ -36,13 +36,19 @@ export default class Server extends Markdown {
 
   // 设置入口文件名
   private entry() {
-    return this.userCfg["entry"] ?? this.globalCfg["defaultEntry"];
+    return (
+      this.userCfg["entry"] ??
+      this.globalCfg["defaultEntry"]
+    );
   }
 
   // 统计Docs文件夹中的文件列表
   private initDocs() {
     // 检查本地是否存在全局的docs文件夹
-    const docsPath = join(this.workspace, this.globalCfg["defaultDocs"]);
+    const docsPath = join(
+      this.workspace,
+      this.globalCfg["defaultDocs"]
+    );
     if (!existsSync(docsPath)) return [];
     return readdirSync(docsPath);
   }
@@ -54,27 +60,26 @@ export default class Server extends Markdown {
   // 主入口映射
   private main() {
     this.router.get("/", ctx => {
-      ctx.status = 201;
+      ctx.status = 200;
       ctx.type = "text/html; charset='utf-8'";
-      ctx.body = this.render(join(this.workspace, this.entry()));
+      ctx.body = this.render(
+        join(this.workspace, this.entry())
+      );
     });
   }
 
   // 系统CSS映射
   private globalCssServer() {
-    if (this.userCfg["useStyle"]) {
-      for (const css in this.globalCfg["defaultCss"]) {
-        this.globalCssRouter.get(css, ctx => {
-          let file = readFileSync(
-            join(this.BeeRoot, this.globalCfg["defaultCss"][css])
-          );
-          ctx.status = 201;
-          ctx.type = "text/css";
-          ctx.body = file.toString();
-        });
-      }
-    }
-    return false;
+    return this.globalCssRouter.get("/default.css", ctx => {
+      ctx.status = 200;
+      ctx.type = "text/css";
+      ctx.body = readFileSync(
+        join(
+          this.BeeRoot,
+          "node_modules/@titor-z/markdown-theme/dist/dist.css"
+        )
+      ).toString();
+    });
   }
 
   // 子文档映射
@@ -84,7 +89,11 @@ export default class Server extends Markdown {
     this.docs.forEach(doc => {
       this.router.get(`/docs/${doc}`, ctx => {
         let mdFile: string = this.render(
-          join(this.workspace, this.globalCfg["defaultDocs"], doc)
+          join(
+            this.workspace,
+            this.globalCfg["defaultDocs"],
+            doc
+          )
         );
         ctx.status = 201;
         ctx.type = "text/html; charset='utf-8'";
@@ -96,17 +105,20 @@ export default class Server extends Markdown {
   // 用户自定义映射
   private route() {
     // 判断用户映射是否存在
-    if ( this.userCfg["routes"] == "{}") return false;
+    if (this.userCfg["routes"] == "{}") return false;
 
     for (const route in this.userCfg["routes"]) {
       this.router.get(route, ctx => {
         let file = readFileSync(
-          join(this.workspace, this.userCfg["routes"][route])
-        )
-        ctx.status = 200
-        ctx.type = "textcss"
-        ctx.body = file.toString()
-      })
+          join(
+            this.workspace,
+            this.userCfg["routes"][route]
+          )
+        );
+        ctx.status = 200;
+        ctx.type = "text/css";
+        ctx.body = file.toString();
+      });
     }
   }
 
@@ -118,15 +130,39 @@ export default class Server extends Markdown {
   protected render(mdFile: string): string {
     let file: Buffer = readFileSync(mdFile);
 
-    if (this.userCfg["useStyle"]) {
-      return `
-        <link rel="stylesheet" href="http://localhost:${this.port()}/editor.css">
-        <link rel="stylesheet" href="http://localhost:${this.port()}/style.css">
-        ${this.md.render(file.toString())}
-      `;
-    }
+    switch (this.userCfg["useStyle"]) {
+      case true:
+        return `
+          <!DOCTYPE html>
+          <html lang="zh-CN">
+          <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+            <meta charset="utf-8">
+            <link rel="stylesheet" href="/default.css">
+          </head>
+          <body>
+            <main class="container">
+              ${this.md.render(file.toString())}
+            </main>
+          </body>
+          </html>`;
+        break;
 
-    return this.md.render(file.toString());
+      default:
+        return `
+        <!DOCTYPE html>
+        <html lang="zh-CN">
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+        <meta charset="utf-8">
+        </head>
+        <body>
+        ${this.md.render(file.toString())}
+        </body>
+        </html>
+        `
+        break;
+    }
   }
 
   // 启动
@@ -134,7 +170,9 @@ export default class Server extends Markdown {
     let Port: number = port ?? this.port();
     if (Port) this.userCfg["port"] = Port;
     this.app.listen(Port, () => {
-      console.log(`🐝 STARTTING...\nOPEN: http://localhost:${Port}/`);
+      console.log(
+        `🐝 STARTTING...\nOPEN: http://localhost:${Port}/`
+      );
     });
   }
 }
